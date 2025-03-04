fe18e23466438a6ae5afcd67f5ba9511b021ce11
John Reis <jhonatareisy7@gmail.com>
2025-03-04 16:51:04 -0300

    feat: add init migration to create users and transactions table

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-const name = ''
+import 'dotenv/config.js'
+import express from 'express'
+
+import { postgresHelper } from './src/db/postgres/helper.js'
+
+const app = express()
+
+app.get('/', async (req, res) => {
+    const results = await postgresHelper.query('SELECT * FROM users;')
+
+    res.send(JSON.stringify(results))
+})
+
+app.listen(3000, () => console.log('listening on port 3000'))
